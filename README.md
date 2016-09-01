9728da4b20cc0689bc65dbfbc7bdf45d7ae4b307
Tarek Hoteit <tarek@nkey.io>
2016-09-01 18:33:27 -0500

    added the scripts: Django app to train the data collected for Stanford Core NLP, reworked the script to extract public stock information from Nasdaq (scripts/all_company_nasdaqcom_importer.py) reworked the script to extract financial data from MorningStar and calculate Altman Zscore (scripts/company_financials_extract.py reworked the script that parses tweets from Twitter for each of the company stock symbols. Added the option to run at specific intervals and list to tweets for a sample size of firms randomly selected from the database.
    Updated the documentation

---
+++ README.md
@@ -0,0 +1,63 @@
+This project is part of my phd dissertation that I published in 2014.
+
+Setup Instructions (in progress)
+
+Setup
+-----
+
+1) install mysql, apache, python3, virtualenv
+
+2) Setup mysql database
+    create database finsentimentdb;
+    create user 'financedbuser'@'localhost' identified by '<password>';
+    grant all privileges on finsentimentdb.* to 'financedbuser'@'localhost';
+    flush privileges;
+   
+3) setup and activate Python3 virtual environment
+    mkdir {env-location} - (select the path where you want the virtual environment)
+    run virtualenv --python=python3 {env-location}
+    source {env-location}/bin/activate
+    
+4) install all project requirements (make sure that you are in the virtual environment
+    pip install -r requirements.txt
+	
+5) download and configure the latest code from github 
+    git clone https://github.com/hoteit/finSentiment.git
+    
+6) update project settings
+    - setup a Twitter app at https://apps.twitter.com (make sure you have a Twitter account first)
+    - get Consumer Key (API Key), Consumer Secret (API Secret), Access Token, 	Access Token Secret
+    and add them in the appropriate location with the settings.py configuration file
+    - update the settings.py file with finSentiment app location in PROJECT_ROOT 
+    and the database password under DATABASES
+    - you can also update the polling time and size for the companies from Twitter by changing
+    tweets_polling_time & tweets_polling_size
+       
+7) setup the application and the database model (make sure to be inside the virtual environment) 
+    ./manage.py makemigrations
+	./manage.py migrate
+
+8) setup apache for production server or you can prepare a local version (TODO : add instructions)
+
+
+Django
+------
+
+from the django environment, create a super user in order to access the site administration.
+Then add a new user with the username "system". This is needed to keep track of the automated 
+user for extracting Twitter data.
+	
+Data Population
+---------------
+step 1 - extract the list of company and stock symbols from Nasdaq website. The script retrieves the stock symbols of all publicly-held companies in the United States 
+and it also adds  major market information.
+    ./manage.py runscript all_company_nasdaqcom_importer
+
+step 2- run the script below to populate the financial data of the firms using last quarterly 
+data extracted from MorningStar.com and Yahoo Finance, calculates Altman Z-Score,
+and stores the results into the database.
+    ./manage.py runscript company_financials_extract
+    
+   
+
+
---
+++ scripts/company_tweets_collect.py
@@ -0,0 +1,144 @@
+__author__ = 'Tarek Hoteit'
+import tweepy
+from tweepy import StreamListener
+from twitterSentiment import models
+from datetime import datetime
+from pytz import timezone
+import json, time, sys
+from numpy import random
+import re
+from django.contrib.auth.models import User
+from twitterSentiment.models import Company
+import pandas as pd
+import schedule
+
+import finSentiment.local_settings as settings
+
+companies = Company.objects.all()  # get the list of companies from the database
+
+stocks = pd.DataFrame(list(companies.values('symbol')))  # get the stock symbols
+stocks['symbol'] = stocks['symbol'].apply(lambda x: str('$'+x.strip()))
+
+tweetsmax = 500  #pick maximum 500 tweets for each company
+tweetscount = 1
+alltweetscount = 1
+iterationcount = 1
+systemid = "system"
+
+
+def run_tweeter_listening():
+    try:
+        stocks_sample_df = stocks.sample(n=settings.tweets_polling_size).to_csv(header=False, line_terminator=',', index=False)
+        twitterStreaming(stocks_sample_df)
+    except:
+        print(sys.exc_info())
+
+def stop_tweeter_listening():
+    print("stop")
+
+
+def timeupdate(twitterdate):
+    # method to return a django-supported time from twitter-based time entry
+    # input comes in the following fashion: Tue Jul 02 14:33:59 +0000 2013
+    # return 2013-06-18 18:23:22-04:00
+    central = timezone('US/Central')
+    return central.localize(datetime.strptime(twitterdate, '%a %b %d %H:%M:%S +0000 %Y'))
+
+
+class twitterListener(StreamListener):
+
+    def __init__(self, api = None, fprefix = 'streamer'):
+        self.api = api or tweepy.API()
+        self.counter = 0
+        return
+
+    def on_data(self, data):
+        global tweetsmax
+        global tweetscount
+        global alltweetscount
+        global iterationcount
+        global systemid
+        print("tweets count:",tweetscount, "/",tweetsmax,". Iteration: ",iterationcount," Total tweets: ", alltweetscount)
+        if tweetsmax == tweetscount:
+            tweetscount=0
+            iterationcount += 1
+            return False
+        else:
+            tweetscount += 1
+            alltweetscount += 1
+        try:
+            tweet = json.loads(data) #convert twitter stream in json into Python dictionary
+            print (tweet)
+            if isinstance(tweet, dict):
+                if tweet['user']['lang'] != 'en': #only store english tweets
+                    return
+                else:
+                    print("tweet: ", tweet['text'])
+                    twitterDatabase(tweet['text'])
+        except:
+            print("Error in Twitter listener. Error message:", sys.exc_info())
+        return
+
+    def on_limit(self, track):
+        print(">> limit")
+        return
+
+    def on_error(self, status_code):
+        print(">>> error: ", str(status_code) + "\n")
+        return
+
+    def on_timeout(self):
+        print(">>> timeout Sleeping for 60 seconds...\n")
+        time.sleep(60)
+        return
+
+def companyNamesbyStocks(tweet):
+    #extract the company behind each stock as the latter is mentioned in a tweet
+    try:
+        match = re.findall("\$\w+",tweet) #captures the stock symbols starting with $, such as $VZ
+        global sp500_companies
+        return ",".join(sp500_companies.Name.get(symbol,symbol)+"("+symbol+")" for symbol in match)
+    except:
+        return "na"
+
+def twitterDatabase(tweet):
+    ## take Twitter financialdata in jsonformat and insert it into the database
+    try:
+        aTweet = models.TwitterText(twitter_user_id=tweet['user']['id'],
+                                    twitter_user_name=tweet['user']['screen_name'],
+                                    twitter_text=tweet['text'],
+                                    twitter_text_id=tweet['id'],
+                                    twitter_text_timestamp=timeupdate(tweet['created_at']),
+                                    #twitter_text_keyword=companyNamesbyStocks(tweet['text']),
+                                    training_user_id=systemid)
+
+        aTweet.save()
+    except:
+        print("Error in Django insert tweet. error message:", sys.exc_info())
+    return
+
+
+def twitterStreaming(stocks):
+    try:
+        print ("test"+settings.consumer_key)
+        auth = tweepy.OAuthHandler(settings.consumer_key, settings.consumer_secret)
+        auth.set_access_token(settings.access_token, settings.access_token_secret)
+        api = tweepy.API(auth)
+        listener = twitterListener(api, "test")
+        print ("Begin Twitter streaming for ", str(stocks).rstrip(','))
+        stream = tweepy.Stream(auth, listener)
+        stream.filter(track=[stocks])
+    except:
+        print ("Error in Twitter streaming",sys.exc_info())
+    return True
+
+
+def run():
+    try:
+        schedule.every(settings.tweets_polling_time).seconds.do(run_tweeter_listening)
+        schedule.every(settings.tweets_polling_time).seconds.do(stop_tweeter_listening)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+    except:
+        print(sys.exc_info())
